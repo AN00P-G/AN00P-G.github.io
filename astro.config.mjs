c4eceb947d0980285af678eef9ed7c8f401ada94
AN00P-G <guragain01@alleghney.edu>
2026-03-02 15:23:08 -0500

    menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
     devToolbar: {
         enabled: false
     },
-    integrations: [
-        react(),
-        mdx()
-    ]
+    integrations: [react(), mdx(), icon()]
 });
